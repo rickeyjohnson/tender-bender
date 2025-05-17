24b0ae9e68a288cdfe2c42b24cc9828b2a5904b4
rickeyjohnson <rdjwinter2004@gmail.com>
2025-05-17 16:06:25 -0400

    Initial Commit

---
+++ TenderBender/TenderBenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenderBenderApp.swift
+//  TenderBender
+//
+//  Created by Rickey Johnson on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TenderBenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
