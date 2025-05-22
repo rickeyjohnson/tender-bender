0baf9ba8fa134dcff745b9d5a4eac14058024347
rickeyjohnson <rdjwinter2004@gmail.com>
2025-05-22 15:22:56 -0400

    roughly finished search tab

--- TenderBender/Views/RestaurantDetailView.swift
+++ TenderBender/Views/RestaurantDetailView.swift
@@ -11,9 +11,45 @@ struct RestaurantDetailView: View {
     let restaurant: Restaurant
     
     var body: some View {
-        VStack {
+        VStack(spacing: 10) {
             Image("tenderImageFake")
                 .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(idealWidth: .infinity, maxWidth: .infinity, maxHeight: 400)
+                .padding(.bottom, 15)
+            
+            VStack(alignment: .center, spacing: 10) {
+                Text(restaurant.name)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                Text(restaurant.location)
+                    .font(.title3)
+                
+                HoursView(opening: restaurant.opening, closing: restaurant.closing)
+                    .font(.caption)
+                    .padding(.bottom)
+                
+                Text(restaurant.description)
+            }
+            .padding(.horizontal, 15)
+            
+            Spacer()
+            
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct HoursView: View {
+    let opening: String
+    let closing: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Mon-Fri:  \(opening) - \(closing)")
+            Text("Sat:  \(opening) - \(closing)")
+            Text("Sun:  \(opening) - \(closing)")
         }
     }
 }
